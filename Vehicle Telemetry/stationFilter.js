b9ef21134efc638ddb2a46991eee8c67052b5684
Jewelljordan <jj.nor25@gmail.com>
2024-12-05 16:24:57 -0600

    Added date filter functionality

--- Vehicle Telemetry/stationFilter.js
+++ Vehicle Telemetry/stationFilter.js
@@ -1,40 +1,41 @@
 function openFilterPopup() {
     document.getElementById("filterPopup").style.display = "block";
-    document.getElementById("grayOverlay").style.display = "block";
+    //document.getElementById("grayOverlay").style.display = "block";
   }
 
   function closeFilterPopup() {
     document.getElementById("filterPopup").style.display = "none";
-    document.getElementById("grayOverlay").style.display = "none";
+    //document.getElementById("grayOverlay").style.display = "none";
   }
 
   function applyFilters() {
-    // Get selected filter values
     const region = document.getElementById("region").value;
     const district = document.getElementById("district").value;
     const state = document.getElementById("state").value;
     const station = document.getElementById("station").value;
 
-    // Create active filter buttons
     addActiveFilter(region);
     addActiveFilter(district);
     addActiveFilter(state);
     addActiveFilter(station);
 
-    // Close the popup
     closeFilterPopup();
 }
 
 function addActiveFilter(filterValue) {
     const activeFiltersContainer = document.querySelector(".active-filters");
 
-    // Check if the filter value is not already active
-    if (!Array.from(activeFiltersContainer.children).some(button => button.textContent === filterValue)) {
+    // check if the filter value is already present in the active filters
+    const existingFilter = Array.from(activeFiltersContainer.children).find(button => 
+        button.textContent.trim().toLowerCase().includes(filterValue.trim().toLowerCase())
+    );
+
+    if (!existingFilter) {
         const filterTag = document.createElement("button");
         filterTag.classList.add("filter-tag");
         filterTag.innerHTML = `${filterValue} <span class="close-filter">✖</span>`;
         
-        // Add event listener to close filter when clicking on the close icon
+        // add event listener to close filter when clicking on the close icon
         filterTag.querySelector(".close-filter").addEventListener("click", function() {
             removeActiveFilter(filterTag);
         });
@@ -46,3 +47,23 @@ function addActiveFilter(filterValue) {
 function removeActiveFilter(filterTag) {
     filterTag.remove();
 }
+
+function openDateFilterPopup() {
+    document.getElementById("dateFilterPopup").style.display = "block";
+  }
+
+  function closeDateFilterPopup() {
+    document.getElementById("dateFilterPopup").style.display = "none";
+  }
+
+  function applyDateFilters() {
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+    
+    const filter = startDate + " - " + endDate;
+    addActiveFilter(filter);
+
+    closeDateFilterPopup();
+  }
+
+  
