6b5c24587d226d3ebd10cf473f1a21e9ecffb71f
Mary Caroline <mr2525@msstate.edu>
2024-12-07 19:28:37 -0600

    updated, route selected view, still issues

--- Vehicle Route/vehicleRouteFilter.js
+++ Vehicle Route/vehicleRouteFilter.js
@@ -67,3 +67,15 @@ function openDateFilterPopup() {
   }
 
   
+
+
+  function showTab(tabId) {
+    const tabs = document.querySelectorAll(".tab");
+    const contents = document.querySelectorAll(".tab-content");
+
+    tabs.forEach(tab => tab.classList.remove("active"));
+    contents.forEach(content => content.classList.remove("active"));
+
+    document.querySelector(`#${tabId}`).classList.add("active");
+    event.target.classList.add("active");
+}
